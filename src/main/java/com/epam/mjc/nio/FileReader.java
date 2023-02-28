3f516b92fc6d4efcf621dd5e2d61441eb4ccfd4f
LoonyJava <loony.java@gmail.com>
2023-02-28 22:09:17 +0500

    Task NIO Task 1 File uploading

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "";
+        Integer age = 0;
+        String email = "";
+        long phone = 0;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsolutePath())))) {
+            String[] nameLine = bufferedReader.readLine().split("Name: ");
+            name = nameLine[1].trim();
+            String[] ageLine = bufferedReader.readLine().split("Age: ");
+            age = Integer.parseInt(ageLine[1].trim());
+            String[] emailLine = bufferedReader.readLine().split("Email: ");
+            email = emailLine[1].trim();
+            String[] phoneLine = bufferedReader.readLine().split("Phone: ");
+            phone = Long.parseLong(phoneLine[1].trim());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
